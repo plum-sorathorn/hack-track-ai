901a87988357b5c1fb4eea24ba5e3d47f309cff7
Sorathorn Thongpitukthavorn <sorathorn.asb@gmail.com>
2025-05-11 12:56:27 +0200

    added functioning hovers for map but will need to adjust for resolution and sizing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
+// App.jsx
 import React, { useState, useEffect } from 'react';
 import DeckGL from '@deck.gl/react';
 import { GeoJsonLayer, ArcLayer } from '@deck.gl/layers';
-import {COORDINATE_SYSTEM} from '@deck.gl/core';
-import { countriesGeoJson, countryCentroids } from './data/countries';
+import { COORDINATE_SYSTEM } from '@deck.gl/core';
+import { countriesGeoJson } from './data/countries';
+import './App.css'
 
 const INITIAL_VIEW_STATE = {
-  longitude: 0,
-  latitude: 0,
-  zoom: 0.75,
+  longitude: 65,
+  latitude: 45,
+  zoom: 0.95,
   pitch: 0,
   bearing: 0
 };
 
 export default function App() {
   const [arcs, setArcs] = useState([]);
+  const [hoveredCountry, setHoveredCountry] = useState(null);
 
   useEffect(() => {
     fetch('/events')
@@ -29,17 +32,31 @@ export default function App() {
       .catch(console.error);
   }, []);
 
+  // Stub for hover handler
+  const handleCountryHover = info => {
+    setHoveredCountry(info.object);
+    console.log('hovered country:', info.object);
+  };
+
   const countryLayer = new GeoJsonLayer({
     id: 'countries',
     data: countriesGeoJson,
     wrapLongitude: true,
     coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
-    stroked:   true,
-    filled:    true,
-    getFillColor: [70, 70, 70],
-    getLineColor: [200, 200, 200],
-    getLineWidth: 1,
+    stroked: true,
+    filled: true,
     pickable: true,
+    autoHighlight: true,
+    highlightColor: [255, 255, 0, 100],
+    onHover: handleCountryHover,
+    getFillColor: feature => {
+      return [70, 70, 70];
+    },
+    updateTriggers: {
+      getFillColor: hoveredCountry
+    },
+    getLineColor: [0, 0, 0, 255],
+    lineWidthMinPixels: 0.5,
   });
 
   const arcLayer = new ArcLayer({
@@ -54,16 +71,18 @@ export default function App() {
   });
 
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller={false}
-      layers={[countryLayer, arcLayer]}
-      style={{
-        width: '100%',
-        height: '100vh',
-        backgroundColor: 'transparent'
-      }}
-      glOptions={{ alpha: true }}
+    <div class="map-container">
+      <DeckGL
+        style={{
+          position: 'absolute',
+          display: 'flex',
+        }}
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={false}
+        interactiveLayerIds={['countries']}
+        layers={[countryLayer, arcLayer]}
+        glOptions={{ alpha: true }}
     />
+    </div>
   );
 }
--- frontend/src/data/countries.js
+++ frontend/src/data/countries.js
@@ -5,11 +5,3 @@ export const countriesGeoJson = feature(
   world110m,
   world110m.objects.countries
 );
-
-export const countryCentroids = {
-  US: [-98.5795, 39.8283],
-  CN: [104.1954, 35.8617],
-  RU: [105.3188, 61.5240],
-  IN: [78.9629, 20.5937],
-  DE: [10.4515, 51.1657],
-};
