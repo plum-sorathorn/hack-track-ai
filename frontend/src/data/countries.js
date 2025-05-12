6f017c43d334af43743e6007e1a673ed693251c4
Sorathorn Thongpitukthavorn <sorathorn.asb@gmail.com>
2025-05-12 15:20:23 +0700

    slight update just to set up for frontend logs and title.

--- frontend/src/data/countries.js
+++ frontend/src/data/countries.js
@@ -3,12 +3,11 @@ import world110m from 'world-atlas/countries-110m.json';
 
 const allCountries = feature(world110m, world110m.objects.countries);
 
-// ISO-3166 numeric codes (change to alpha-2/3 or names if you prefer)
 const BLACKLIST = [10]
 
 export const countriesGeoJson = {
   type: 'FeatureCollection',
   features: allCountries.features.filter(
-    f => !BLACKLIST.includes(+f.id)    // ← keep everything *not* on the list
+    f => !BLACKLIST.includes(+f.id)
   )
 };
